4a8a66c722c5ac97735cb57dcf0d2356f63ca2b3
seema.pandey2483@gmail.com <seema.pandey2483@gmail.com>
2017-10-17 15:48:32 +0530

    zdas

--- src/com/teamup/agencyportal/controller/AdminController.java
+++ src/com/teamup/agencyportal/controller/AdminController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired ;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
